6e383c7d6e41050c75d147b972418ff5e4147398
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-03-20 23:24:06 +0530

    Fix server setup by defining PORT variable and correcting API route integration for students

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const PORT = process.env.PORT || 8000
 
+//cors Middleware used ensuring cross domain communication
 app.use(cors());
+
+//express.json is also a middleware used to parse the incoming request
 app.use(express.json());
 
-app.use("/api/students");
+
+app.use("/api/students",StudentRoutes);
+
 
 app.listen("PORT", () => {
     console.log(`Server is running on Port ${PORT}`) 
